898231eefdb3c35a143a5dbf9985faf6a09a4718
remsky <jeremy.braun@ucalgary.ca>
2025-01-12 21:34:13 -0700

    fix: update .gitignore and suppress erramt Gradio warnings in app.py

--- ui/app.py
+++ ui/app.py
@@ -1,3 +1,9 @@
+import warnings
+
+# Filter out Gradio Dropdown warnings about values not in choices
+#TODO: Warning continues to be displayed, though it isn't breaking anything
+warnings.filterwarnings('ignore', category=UserWarning, module='gradio.components.dropdown')
+
 from lib.interface import create_interface
 
 if __name__ == "__main__":
