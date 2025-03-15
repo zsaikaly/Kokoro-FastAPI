22185dbc8971137035990814b5296f80ca8f217b
Fireblade <fireblade5234@gmail.com>
2025-03-15 16:35:06 -0400

    Fixed the ps1 files downloading packages before the venv and made all starrt scripts auto download models

--- start-gpu.ps1
+++ start-gpu.ps1
@@ -8,10 +8,6 @@ $Env:MODEL_DIR="src/models"
 $Env:VOICES_DIR="src/voices/v1_0"
 $Env:WEB_PLAYER_PATH="$Env:PROJECT_ROOT/web"
 
-uv pip install wheel setuptools ninja typing_extensions>=4.10.0 fastapi uvicorn
-
-uv pip install torch==2.6.0+cu124 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 uv pip install -e ".[gpu]"
-
-
+uv run --no-sync python docker/scripts/download_model.py --output api/src/models/v1_0
 uv run --no-sync uvicorn api.src.main:app --host 0.0.0.0 --port 8880
--- start-gpu.sh
+++ start-gpu.sh
@@ -12,5 +12,7 @@ export VOICES_DIR=src/voices/v1_0
 export WEB_PLAYER_PATH=$PROJECT_ROOT/web
 
 # Run FastAPI with GPU extras using uv run
+# Note: espeak may still require manual installation,
 uv pip install -e ".[gpu]"
+uv run --no-sync python docker/scripts/download_model.py --output api/src/models/v1_0
 uv run --no-sync uvicorn api.src.main:app --host 0.0.0.0 --port 8880
